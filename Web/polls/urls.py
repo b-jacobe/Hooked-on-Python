c5315bd395f17b7856647f7e2f88608a5a86e376
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2019-12-24 17:17:14 -0600

    Updated django polls project. Added 404 error if poll question does not exist.

--- Web/polls/urls.py
+++ Web/polls/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/, /polls/5/, /polls/5/results/, /polls/5/vote/
     path('',views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/',views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Web/polls/views.py
+++ Web/polls/views.py
@@ -1,5 +1,37 @@
+#from django.shortcuts import render
+#from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+#from django.template import loader
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.http import Http404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Poll Index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html',context)
+
+#def index(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {
+        #'latest_question_list': latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+    #output = ', '.join([q.question_text for q in latest_quesiton_list])
+    #return HttpResponse(output)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exit")
+    return render(request, 'polls/detail.html',{'question':question})
+    #return HttpResponse("Currently viewing question %s.")
+
+def results(request, question_id):
+    response = ("Currently viewing results of question %s.")
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting for question %s." % question_id)
