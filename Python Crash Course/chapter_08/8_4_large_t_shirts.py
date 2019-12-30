633e4cd64089958178641accedc527933e69ca9f
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2019-12-29 22:45:06 -0800

    Update 8_4_large_t_shirts.py
    Cleaned up make_shirt function

--- Python Crash Course/chapter_08/8_4_large_t_shirts.py
+++ Python Crash Course/chapter_08/8_4_large_t_shirts.py
@@ -7,13 +7,14 @@ medium shirt with the default message, and a shirt of any size with a different
 message.
 
 Created: 2-10-19
+Updated: 12-29-19
 @author: Brian Jacobe
 
 """
 
 def make_shirt(size="large", message="I love Python"):
-	print("Shirt Size: " + size)
-	print("Shirt Message: " + message + "\n")
+  result = "Shirt Size: " + size + "\n" + "Shirt Message: " + message
+  return print(result)
 
 make_shirt()
 make_shirt("medium")
