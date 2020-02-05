6f11d61f6d3275c39955804ac6061ff3dac55e93
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2020-02-04 18:45:49 -0800

    Create foobar.py
    Continuation of Python review. Created a simple foobar divisible module that takes in user input and outputs foo, bar, foobar, or the integer value.

---
+++ Python Crash Course/review/foobar.py
@@ -0,0 +1,24 @@
+"""
+foo_bar.py
+
+Print numbers from 1 to user input value,
+assuming its an integer. For every multiple
+of 5, print 'Foo', every multiple of 7, print
+'Bar', and print 'FooBar' if its a multiple
+of both. Otherwise print the number.
+"""
+
+def foo_bar():
+  user_value = int(input("Enter a numeric value.\n"))
+  for n in range(1, user_value+1):
+    if n % 3 == 0:
+      print("Foo")
+    elif n % 5 == 0:
+      print("Bar")
+    elif n % 3 == 0 and n % 5 == 0:
+      print("FooBar")
+    else:
+      print(n)
+
+# Driver Code
+foo_bar()
